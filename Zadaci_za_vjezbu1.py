1e424323f760e4e652f776ede715d87af9afed5c
abobicanec <abobicane@student.unipu.hr>
2025-11-15 17:34:16 +0100

    dorada

--- Zadaci_za_vjezbu1.py
+++
@@ -1,6 +0,0 @@
-def kvadriraj(x):
-    return x ** 2
-
-kvadriraj = lambda x: x **2
-
-print(kvadriraj(4))
--- Zadatak_za_vjezbu2.py
+++
@@ -1,5 +0,0 @@
-def zbroji_pa_kvadriraj(a,b):
-    return (a+b) **2
-
-zbroji_pa_kvadriraj = lambda a,b: (a+b) **2
-print(zbroji_pa_kvadriraj(1,3))
